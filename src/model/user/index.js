3536c2186042ec742e431f67fb84951b4dc62d31
YSengar02 <yassi.sengar@ens.enterprises>
2023-09-22 00:22:29 +0530

    make jwt token and usersignup app

---
+++ src/model/user/index.js
@@ -0,0 +1,33 @@
+import { DataTypes } from "sequelize";
+import {sequelize} from "../../db/index.js";
+
+const User  = sequelize.define('users',{
+    id:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name:{
+        type: DataTypes.STRING,    
+    },
+    email:{
+        type: DataTypes.STRING
+    },
+    contact_no:{
+        type: DataTypes.STRING
+    },
+    password:{
+        type: DataTypes.STRING
+    },
+    status:{
+        type: DataTypes.BOOLEAN
+    }
+
+},{
+    timestamps:true,
+    createdAt:"created_At",
+    updatedAt:"updated_At"
+}
+)
+
+export default User;
---
+++ src/routes/users_api/middleware/index.js
@@ -0,0 +1,30 @@
+import { Jwt } from "jsonwebtoken";
+
+export const signJwtToken= async(data)=>{
+    const token = jwt.sign({
+        data 
+    },"12345",{expiresIn:'8hr'});
+    return token
+}
+
+export const headerverify= async(req,resp,next)=>{
+    try {
+        const decode= jwt.verify(req.headers.authorization,'secret')
+        if(decode){
+            next();
+        }
+        else{
+            resp.status(200).json({
+                message:"A token is require for authentication",
+                status: 403
+            })
+        }
+
+    } catch (error) {
+        resp.status(200).json({
+            message:"Invalid Token",
+            status:400
+        })
+        
+    }
+}
